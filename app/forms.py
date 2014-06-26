8bc6d9ce1b015dc5df7b259cec099ca8f04f8b35
Carlos Cunha <carloscunha47@gmail.com>
2014-06-26 14:10:16 -0700

    opening page improvements

--- app/forms.py
+++ app/forms.py
@@ -3,10 +3,10 @@ from wtforms import TextField, BooleanField, SelectField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    start = TextField('start', validators = [Required()])
+    start = TextField('Start:', validators = [Required()])
     #openid = TextField('openid', validators = [Required()])
-    end = TextField('end', validators = [Required()])
+    end = TextField('End:', validators = [Required()])
     
-    place = SelectField('stops', choices = [('restaurant','Restaurant'),('gas','Gas station'),('hotel','Hotel'),('drug','Pharmacy')])
-    partroute=SelectField('partroute', choices = [('middle','middle'),('beginning','beginning'),('end','end')],default=('middle','middle'))
+    place = SelectField('Stop on the way:', choices = [('restaurant','Restaurant'),('gas','Gas station'),('hotel','Hotel'),('drug','Pharmacy')])
+    partroute=SelectField('When to stop:', choices = [('middle','middle'),('beginning','beginning'),('end','end')],default=('middle','middle'))
     remember_me = BooleanField('remember_me', default = False)
