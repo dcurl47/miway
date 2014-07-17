20c83f98ab42ece4acf5b6286f3dfdfaf3ede66b
cec <carloscunha47@gmail.com>
2014-07-17 11:26:14 -0700

    Update keys.py

--- app/keys.py
+++ app/keys.py
@@ -1,9 +1,9 @@
 class keys():
-    google='AIzaSyAjcoHKuP8mS6OsIcjkRCypwkL-Cb5WccQ'
-    mapquest='Fmjtd%7Cluur2g6znl%2C70%3Do5-9a8w9y'
+    google='get_your_own'
+    mapquest='get_your_own'
     
-    yelp_ywsid='OMOWBvBv7K3BjO6UebGMlw'
-    yelp_cons_key='ttyV6ZPYVpkyCAc7YyCzow'
-    yelp_cons_sec='soI3udODLTTtXImUvN8o2C45V64'
-    yelp_tok='BPV7EkOMMoDHYmuj1XksQTOGRZUv97Jt'
-    yelp_tok_sec='3MN7W_lO3IhCafDu_Tef-wcEoog'
+    yelp_ywsid='get_your_own'
+    yelp_cons_key='get_your_own'
+    yelp_cons_sec='get_your_own'
+    yelp_tok='get_your_own'
+    yelp_tok_sec='get_your_own'
